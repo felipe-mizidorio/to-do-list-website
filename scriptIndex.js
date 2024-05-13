421d6faabf6156b3690c08b3127b3bd132db2897
lucasmoraesb <luca.m.borges@gmail.com>
2024-05-13 15:59:34 -0300

    feat: remove tasks

--- scriptIndex.js
+++ scriptIndex.js
@@ -10,7 +10,7 @@ document.getElementById('exit').addEventListener('click', function(event) {
 
 document.getElementById('form1').addEventListener('submit', adicionarTarefa);
 const userData = JSON.parse(localStorage.getItem(localStorage.getItem('userLogged')));
-const notesList = []
+let notesList = []
 const tabela = document.getElementById('table1');
 const tarefasSalvas = userData.notes;
 if(tarefasSalvas){
@@ -19,6 +19,11 @@ if(tarefasSalvas){
         notesList.push(linhaTarefa);
     });
 }
+
+const botoesDelete = document.querySelectorAll('.btn-delete');
+botoesDelete.forEach(botaoDelete => {
+    botaoDelete.addEventListener('click', excluirTarefa);
+    });
 
 function adicionarTarefa(e) {
     e.preventDefault();
@@ -31,6 +36,21 @@ function adicionarTarefa(e) {
     userData.notes = notesList;
     saveData();
     renderizarListas(newNote);
+    const botoesDelete = document.querySelectorAll('.btn-delete');
+    botoesDelete.forEach(botaoDelete => {
+    botaoDelete.addEventListener('click', excluirTarefa);
+    });
+}
+
+function excluirTarefa(event) {
+    // Adicione um evento de clique a cada botão de exclusão
+    const trPai = event.target.closest('tr');
+    const primeiroFilho = trPai.firstElementChild;
+    notesList = notesList.filter(item => item.uuid !== primeiroFilho.textContent);
+    userData.notes = notesList;
+    saveData();
+    // Remova o elemento pai (o <td>)
+    trPai.remove();
 }
 
 function renderizarListas(notesList){
@@ -39,14 +59,22 @@ function renderizarListas(notesList){
     let td2 = document.createElement('td');
     let td3 = document.createElement('td');
     let td4 = document.createElement('td');
+    let td5 = document.createElement('td');
     td1.textContent = notesList.uuid;
     td2.textContent = notesList.titulo;
     td3.textContent = notesList.dataCriacao;
+    td4.innerHTML = `<button class="btn-delete"> 
+                        <img src="material-symbols_delete.png">
+                    </button>`;
+    td5.innerHTML = `<button class="btn-edit"> 
+                        <img src="tabler_edit.png">
+                    </button>`;
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
+    tr.appendChild(td5);
     tabela.appendChild(tr);
 }
 
